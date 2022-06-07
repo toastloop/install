ff4799e8f74cff730da46a66ecea298b6a41a972
toastloop <toastloop@gmail.com>
2022-06-07 17:30:01 -0600

    Added options for different configs

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Please choose an option:"
+echo "1. Desktop"
+echo "2. Server"
+echo "3. Xorg"
+echo "4. Minimal"
+while read -p "Please select an option [1-4] " option
+do
+case $option in
+    [1]* ) archinstall --config desktop.json --creds creds.json --disk_layouts disk.json;break;;
+    [2]* ) archinstall --config server.json --creds creds.json --disk_layouts disk.json;break;;
+    [3]* ) archinstall --config xorg.json --creds creds.json --disk_layouts disk.json;break;;
+    [4]* ) archinstall --config minimal.json --creds creds.json --disk_layouts disk.json;break;;
+    "" ) archinstall --config minimal.json --creds creds.json --disk_layouts disk.json;;
+    * ) echo "Error: Please enter a number between 1 and 4.";;
+esac
+done
