eaa458759a9624e503982b88d115ebe139c9334f
toastloop <toastloop@gmail.com>
2022-06-07 18:18:41 -0600

    added a script path variable

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export SCRIPT="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cp pacman.conf /etc/pacman.conf
 
@@ -15,11 +16,11 @@ echo "4. Minimal"
 while read -p "Please select an option [1-4] " option
 do
 case $option in
-    [1]* ) archinstall --config desktop.json --creds creds.json --disk_layouts disk.json;break;;
-    [2]* ) archinstall --config server.json --creds creds.json --disk_layouts disk.json;break;;
-    [3]* ) archinstall --config xorg.json --creds creds.json --disk_layouts disk.json;break;;
-    [4]* ) archinstall --config minimal.json --creds creds.json --disk_layouts disk.json;break;;
-    "" ) archinstall --config minimal.json --creds creds.json --disk_layouts disk.json;;
+    [1]* ) archinstall --config $SCRIPT/desktop.json --creds $SCRIPT/creds.json --disk_layouts $SCRIPT/disk.json;break;;
+    [2]* ) archinstall --config $SCRIPT/server.json --creds $SCRIPT/creds.json --disk_layouts $SCRIPT/disk.json;break;;
+    [3]* ) archinstall --config $SCRIPT/xorg.json --creds $SCRIPT/creds.json --disk_layouts $SCRIPT/disk.json;break;;
+    [4]* ) archinstall --config $SCRIPT/minimal.json --creds $SCRIPT/creds.json --disk_layouts $SCRIPT/disk.json;break;;
+    "" ) archinstall --config $SCRIPT/minimal.json --creds $SCRIPT/creds.json --disk_layouts $SCRIPT/disk.json;;
     * ) echo "Error: Please enter a number between 1 and 4.";;
 esac
 done
